2b4da767f1fa84a51a30bb3d363d0777ace73129
HendelRamzi <h.ramzi.mi@gmail.com>
2026-02-22 22:02:33 +0100

    Fixed the seed user error, and added the redirect to admin panel route

--- database/seeders/SuperAdminSeeder.php
+++ database/seeders/SuperAdminSeeder.php
@@ -2,12 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Enums\UserRole;
 use App\Models\Tool;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Artisan;
-use Password;
+use Illuminate\Support\Facades\Hash;
 
 
 // Create the super admin user
@@ -21,13 +22,13 @@ class SuperAdminSeeder extends Seeder
        $data = [
             'name' => "admin", 
             "email" => "admin@admin.com",
-            'password' => Password::hash('admin'), 
+            'password' => Hash::make('admin'), 
        ]; 
 
        $super_admin = User::create($data);
 
 
         //Make the user as super_admin role
-        Artisan::call('shield:super-admin --user='. $super_admin->id);
+        $super_admin->assignRole(UserRole::super_admin);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use App\Models\File;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/debug', function () {
+Route::get('/', function () {
+    return redirect()->to('/admin');
 });
